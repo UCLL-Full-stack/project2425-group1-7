380ed960699b923ddea6417d03cd942ab35ef8bb
AdamBenkhazzi03 <adam.benkhazzi@student.ucll.be>
2024-12-18 19:45:14 +0100

    feat[view_feed]:> >1. feed view updated >2. separated review details page into a component

---
+++ front-end/components/reviews/reviewDetails.tsx
@@ -0,0 +1,153 @@
+import AlbumCard from "@/components/album/albumCard";
+import IconComment from "@/components/ui/comment";
+import IconLike from "@/components/ui/like";
+import IconDisc from "@/components/ui/loading";
+import albumService from "@/services/albumService";
+import reviewService from "@/services/reviewService";
+import { Album, User, Review } from "@/types/index";
+import { Rating } from "@mui/material";
+import Link from "next/link";
+import { useEffect, useState } from "react";
+
+type Props={
+    user: User,
+    review: Review,
+    handleClickComment: (id: number)=>void
+}
+
+const ReviewDetails = ({ user, review, handleClickComment }: Props) => {
+    const [album, setAlbum] = useState<Album | null>(null);
+    const [isLiked, setIsLiked] = useState<boolean>(false);
+    const [likeCount, setLikeCount] = useState<number>(0);
+    const [isLoading, setIsLoading] = useState<boolean>(true);
+    const [error, setError] = useState<string>("");
+    const [clicked, setClicked] = useState<boolean>(false);
+
+    useEffect(() => {
+        if (!review) {
+            setIsLoading(false);
+            return;
+        }
+
+        const fetchAlbum = async () => {
+            try {
+                const details = review.albumId.split("_");
+                const fetchedAlbum = await albumService.fetchAlbum(details[0], details[1]);
+                setAlbum(fetchedAlbum);
+            } catch (err) {
+                setError("Error fetching album");
+            } finally {
+                setIsLoading(false);
+            }
+        };
+
+        fetchAlbum();
+
+        const userLiked = review.likes.find((like) => like === user.id);
+        setIsLiked(!!userLiked);
+        setLikeCount(review.likes.length);
+    }, [review, user]);
+
+    useEffect(() => {
+        if (!review || !user?.id || !clicked) return;
+
+        const updateLikes = async () => {
+            try {
+                const response = isLiked
+                    ? await reviewService.likeReview(review.id)
+                    : await reviewService.unlikeReview(review.id);
+                if (!response.ok) {
+                    throw new Error("Error updating likes");
+                }
+            } catch (err) {
+                setError("Error updating likes");
+            }
+        };
+
+        updateLikes();
+
+        if (isLiked) {
+            review.likes.push(user.id);
+        } else {
+            review.likes = review.likes.filter((like) => like !== user.id);
+        }
+        setLikeCount(review.likes.length);
+    }, [isLiked]);
+
+    const handleLike = () => {
+        setClicked(true);
+        setIsLiked(!isLiked);
+    };
+
+    if (isLoading) {
+        return (
+            <div className="flex justify-center items-center">
+                <IconDisc height={100} width={100} />
+            </div>
+        );
+    }
+
+    if (error) {
+        return <span className="text-red-800 main-font">{error}</span>;
+    }
+
+    if (!user || !album || !review) {
+        return null;
+    }
+
+    return (
+        <div className="max-w-4xl mx-auto bg-text1 py-4 px-6 rounded-lg shadow-md">
+            <div className="grid grid-cols-4">
+                <div className="flex justify-center flex-wrap">
+                    <AlbumCard album={album} />
+                    <Rating className="text-center p-2" size="large" readOnly value={review.starRating} />
+                </div>
+                <div className="col-span-3 p-4">
+                    <div className="flex justify-between items-baseline ">
+                        <h1 onClick={()=>handleClickComment(review.id)} className="hover:scale-105 text-4xl font-bold mb-4 text-text2 duration-100 truncate">{review.title}</h1>
+                        <div className="mb-4 flex gap-2">
+                            <h2 className="text-xl main-thin text-text2">By</h2>
+                            <Link
+                                href={`/profile/${review.author.id}`}
+                                className={`${
+                                    review.author.id === user.id && "text-green-500"
+                                } text-xl main-font text-bg2 hover:text-text2 hover:scale-105 duration-100`}
+                            >
+                                {review.author.username ?? "Unknown"}
+                            </Link>
+                        </div>
+                    </div>
+                    <div className="m-5">
+                        <p className="main-thin text-md text-bg2">{review.body}</p>
+                    </div>
+                </div>
+            </div>
+            <div className="flex items-start justify-end gap-2 text-xs sm:text-sm text-text2 main-font">
+                <span className="flex items-center gap-2 text-xs sm:text-sm text-text2 main-font">
+                    <p> {likeCount} </p>
+                    <IconLike
+                        width={25}
+                        height={25}
+                        className={`duration-100 hover:scale-105 ${
+                            isLiked
+                                ? "text-green-500 hover:text-red-500 "
+                                : "text-text2 hover:text-green-500 "
+                        }`}
+                        onClick={handleLike}
+                    />
+                </span>
+                <span className="flex items-center gap-2 text-xs sm:text-sm text-text2 main-font">
+                    <p>{review.comments.length}</p>
+                    <IconComment
+                        onClick={()=>handleClickComment(review.id)}
+                        className="hover:scale-110 duration-100 text-text2 hover:text-bg1"
+                        width={25}
+                        height={25}
+                    />
+                </span>
+            </div>
+        </div>
+    );
+};
+
+export default ReviewDetails;
