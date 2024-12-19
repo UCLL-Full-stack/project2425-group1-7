18c9f5a58fb98666341c720a76e99c2ed1b4e508
AdamBenkhazzi03 <adam.benkhazzi@student.ucll.be>
2024-12-19 22:00:29 +0100

    fix[i18n_about_us]: switched buttons to selects

--- front-end/pages/about/index.tsx
+++ front-end/pages/about/index.tsx
@@ -1,17 +1,15 @@
 import Head from "next/head";
-import '../../i18n'
+import '../../i18n';
 import { useTranslation } from 'react-i18next';
 
-const About = ()=>{
+const About = () => {
     const { t, i18n } = useTranslation();
 
     const changeLanguage = (lng: string) => {
         i18n.changeLanguage(lng);
     };
 
-    t();
-
-    return(
+    return (
         <>
             <Head>
                 <title>Yadig?</title>
@@ -20,14 +18,20 @@ const About = ()=>{
                 <main className="flex-1 flex flex-col justify-center items-center gap-3 bg-bg1 p-10 overflow-y-auto">
                     <span className="text-text2 text-3xl main-font">{t('thanks')}</span>
                     <span className="text-text2 max-w-[50vw] text-center text-xl main-thin">{t('story')}</span>
-                    <div className="rounded-md bg-text1 text-text2 flex p-5 gap-5 items-center ">
-                        <button  onClick={()=>changeLanguage('en')}>English</button>
-                        <button onClick={()=>changeLanguage('fr')}>Français</button>
+                    <div className="flex">
+                        <select 
+                            className="rounded-md bg-text1 text-text2 p-5"
+                            onChange={(e) => changeLanguage(e.target.value)}
+                        >
+                            <option value="en">English</option>
+                            <option value="fr">Français</option>
+                        </select>
                     </div>
                 </main>
             </div>
         </>
-    )
-}
+    );
+};
 
 export default About;
+
